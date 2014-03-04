7c94b21922c16830d143057f9130c6e7406a7aac
jamesv28 <james.volmert@gmail.com>
2014-03-04 13:12:41 -0700

    Initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,28 @@
+class StaticPagesController < ApplicationController
+
+  def home
+
+  end
+
+  def diabetes
+
+  end
+
+  def publications
+
+  end
+
+
+  def exercise
+  end
+
+  def related_articles
+  end
+
+  def recipes
+
+  end
+
+  def nutrition_news
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiabetesHealth::Application.config.secret_token = '13d8c3cf218cb673c818a0668862fa130ff000a78e3c76a2ae0fb69c21e932b6508963587790f7f59fe6f31cc962a9fb963efa8ad2defd9e1a0c92a54639269b'
